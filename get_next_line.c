115a0c1a1adef8039823ead0dca96334c04c35bb
pabonnet <pabonnet@student.42.fr>
2019-03-06 18:31:04 +0100

    push

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pabonnet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 14:55:26 by pabonnet          #+#    #+#             */
-/*   Updated: 2019/03/06 18:22:19 by pabonnet         ###   ########.fr       */
+/*   Updated: 2019/03/06 18:30:52 by pabonnet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@
 #include "Libft/libft.h"
 #include "get_next_line.h"
 
-int		malloc_env(t_env *env)
+int		malloc_env(t_env **env)
 {
-	if (!(env = malloc(sizeof(t_env))))
+	if (!(*env = malloc(sizeof(t_env))))
 		return (-1);
-	env->buf = 0;
-	env->pos = 0;
-	env->read_len = 0;
+	(*env)->buf = 0;
+	(*env)->pos = 0;
+	(*env)->read_len = 0;
 	return (1);
 }
 
@@ -33,12 +33,12 @@ int		get_next_line(const int fd, char **line)
 	int		i;
 
 	i = 0;
-	if (!env && !malloc_env(env))
+	if (!env && !malloc_env(&env))
 		return (-1);
 	if (fd == -1)
 		return (0);
 	printf("a\n");
-	while (((env->read_len = read(fd, env->buf, BUFF_SIZE))) > 0)
+	while ((env->read_len = read(fd, env->buf, BUFF_SIZE)) > 0)
 	{
 		printf("b\n");
 		env->buf[env->read_len] = '\0';
@@ -49,6 +49,7 @@ int		get_next_line(const int fd, char **line)
 			break;
 		printf("%d\n", env->read_len);
 	}
+	printf("c\n");
 	*line = ft_strsub(env->buf, env->pos, i - env->pos);
 		printf("%s\n", *line);
 	return (0);
